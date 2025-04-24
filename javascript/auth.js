1e0c458244e0fcb6c604cc4995c9e01ce28edd65
Robert Oganisyan <rob.oganisyan@inbox.ru>
2025-04-24 18:58:14 +0300

    Страницы входа и регистрации

---
+++ javascript/auth.js
@@ -0,0 +1,4 @@
+// Переход на страницу входа
+document.getElementById('loginBtn').addEventListener('click', () => {
+    window.location.href = 'login.html'; // Перенаправляем на страницу входа
+  });
